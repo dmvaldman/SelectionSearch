2fc1b6ac870ccb032296b72e2e8097bf7891527b
David Valdman <dmvaldman@gmail.com>
2023-05-06 16:38:38 -0700

    First version ready

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,4 @@
-const METAPHOR_SCORE_THRESHOLD = 0.19
+const METAPHOR_SCORE_THRESHOLD = 0.16
 let currEl = null
 
 function handleRightClick(event) {
@@ -12,42 +12,128 @@ function createLinkEl(link){
     const linkItem = document.createElement('div');
     linkItem.classList.add('link-item');
 
+    // Put link title and date in a container
+    const linkTitleDateContainer = document.createElement('div');
+    linkTitleDateContainer.classList.add('link-title-date-container');
+    linkItem.appendChild(linkTitleDateContainer);
+
     const linkTitle = document.createElement('a');
     linkTitle.classList.add('link-title');
     linkTitle.href = link.url;
     linkTitle.textContent = link.title;
-    linkItem.appendChild(linkTitle);
+
+    // put the URL domain next to it in a span
+    const linkDomain = document.createElement('span');
+    linkDomain.classList.add('link-domain');
+    linkDomain.textContent = new URL(link.url).hostname + ' | ' + link.dateCreated;
+
+    // put date flush to the right
+    // const linkDate = document.createElement('span');
+    // linkDate.classList.add('link-date');
+    // linkDate.textContent = link.dateCreated;
+
+    linkTitleDateContainer.appendChild(linkTitle);
+    linkTitleDateContainer.appendChild(linkDomain);
+    // linkTitleDateContainer.appendChild(linkDate);
 
     const linkDescription = document.createElement('span');
     linkDescription.classList.add('link-description');
     linkItem.appendChild(linkDescription);
+
+    const tagContainer = document.createElement('div');
+    tagContainer.classList.add('tag-container');
+    linkItem.appendChild(tagContainer);
+
+    const carouselContainer = document.createElement('div');
+    carouselContainer.classList.add('carousel-container');
+    linkItem.appendChild(carouselContainer);
+
+    const carouselContent = document.createElement('div');
+    carouselContent.classList.add('carousel-content');
+    carouselContainer.appendChild(carouselContent);
 
     const linkScore = document.createElement('span');
     linkScore.classList.add('link-score');
     linkScore.textContent = link.score;
     linkItem.appendChild(linkScore);
 
-    const linkDate = document.createElement('span');
-    linkDate.classList.add('link-date');
-    linkDate.textContent = link.dateCreated;
-    linkItem.appendChild(linkDate);
-
     return linkItem
+}
+
+function createTagEls(linkItemEl, tags){
+    if (tags.length == 0) return
+
+    const tagContainer = linkItemEl.querySelector('.tag-container');
+
+    // truncate to three tags
+    if (tags.length > 3) tags.splice(3)
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span');
+        tagEl.classList.add('link-tag');
+        tagEl.textContent = tag;
+        tagContainer.appendChild(tagEl);
+    });
 }
 
 function createLinkContainerEl(currEl){
     // create a tooltip over the current element
+    // contains draggable bar, wrapper content, and resize bar
     const tooltipContainer = document.createElement('div')
     tooltipContainer.classList.add('tooltip')
 
-    const linkList = document.createElement('div')
-    linkList.classList.add('link-list')
+    // create top portion with close bar and draggable bar
+    const topBar = document.createElement('div')
+    topBar.classList.add('top-bar')
+
+    // contains the content of the tooltip
+    const tooltipContainerWrapper = document.createElement('div')
+    tooltipContainerWrapper.classList.add('tooltip-wrapper')
+
+    // add resize div
+    const resizeHandle = document.createElement('div')
+    resizeHandle.textContent = '↘'
+    resizeHandle.classList.add('resize-handle')
 
     const closeEl = document.createElement('button')
     closeEl.classList.add('close-tooltip')
-    closeEl.textContent = 'X'
+    closeEl.textContent = '×'
 
-    linkList.appendChild(closeEl)
+    topBar.appendChild(closeEl)
+    tooltipContainer.appendChild(topBar)
+    tooltipContainer.appendChild(tooltipContainerWrapper)
+    tooltipContainer.appendChild(resizeHandle)
+
+    const linkList = document.createElement('div')
+    linkList.classList.add('link-list')
+    tooltipContainerWrapper.appendChild(linkList)
+
+    let mousePosition = {
+        x: 0,
+        y: 0
+    }
+
+    function moveTooltip(e){
+        const dx = e.clientX - mousePosition.x
+        const dy = e.clientY - mousePosition.y
+        const left = parseInt(window.getComputedStyle(tooltipContainer).left)
+        const top = parseInt(window.getComputedStyle(tooltipContainer).top)
+        tooltipContainer.style.left = `${left + dx}px`
+        tooltipContainer.style.top = `${top + dy}px`
+        mousePosition.x = e.clientX
+        mousePosition.y = e.clientY
+    }
+
+    // make topbar draggable
+    topBar.addEventListener('mousedown', function(e){
+        mousePosition.x = e.clientX
+        mousePosition.y = e.clientY
+        document.addEventListener('mousemove', moveTooltip)
+    })
+
+    document.addEventListener('mouseup', function(){
+        document.removeEventListener('mousemove', moveTooltip)
+    })
 
     // Function to close the tooltip when the "X" button is clicked
     function closeTooltip() {
@@ -60,7 +146,81 @@ function createLinkContainerEl(currEl){
     tooltipContainer.style.left = `${currEl.clientX}px`;
     tooltipContainer.style.top = `${currEl.clientY}px`;
 
+
+    let isResizing = false;
+    let initialWidth, initialHeight, initialMouseX, initialMouseY;
+
+    resizeHandle.addEventListener("mousedown", (e) => {
+        isResizing = true;
+        initialWidth = tooltipContainer.offsetWidth;
+        initialHeight = tooltipContainer.offsetHeight;
+        initialMouseX = e.clientX;
+        initialMouseY = e.clientY;
+    });
+
+    window.addEventListener("mousemove", (e) => {
+        if (!isResizing) return;
+
+        const newWidth = initialWidth + (e.clientX - initialMouseX);
+        const newHeight = initialHeight + (e.clientY - initialMouseY);
+
+        tooltipContainer.style.width = newWidth + "px";
+        tooltipContainer.style.height = newHeight + "px";
+    });
+
+    window.addEventListener("mouseup", () => {
+        isResizing = false;
+    });
+
     return {tooltipContainer, linkList}
+}
+
+function createCarouselEl(linkItem, quotes){
+    if (quotes.length == 0) return
+
+    const carouselContainer = linkItem.querySelector('.carousel-container');
+    const carouselContent = carouselContainer.querySelector('.carousel-content');
+
+    const prevButton = document.createElement('button');
+    prevButton.classList.add('carousel-left');
+    prevButton.textContent = '❮';
+    carouselContainer.insertBefore(prevButton, carouselContainer.firstChild);
+
+    quotes.forEach((quote) => {
+        const quoteEl = document.createElement('div');
+        quoteEl.classList.add('carousel-item');
+        carouselContent.appendChild(quoteEl);
+        // truncate after 40 words
+        if (quote.split(' ').length > 50)
+            quote = quote.split(' ').slice(0, 50).join(' ') + '...';
+        quoteEl.textContent = '"' + quote + '"';
+    });
+
+    const nextButton = document.createElement('button');
+    nextButton.classList.add('carousel-right');
+    nextButton.textContent = '❯';
+    carouselContainer.appendChild(nextButton);
+
+    let currentItemIndex = 0;
+    const totalItems = quotes.length;
+
+    const updateCarousel = () => {
+        carouselContent.style.transform = `translateX(-${currentItemIndex * 100}%)`;
+        prevButton.disabled = currentItemIndex === 0;
+        nextButton.disabled = currentItemIndex === totalItems - 1;
+    };
+
+    prevButton.addEventListener('click', () => {
+        currentItemIndex--;
+        updateCarousel();
+    });
+
+    nextButton.addEventListener('click', () => {
+        currentItemIndex++;
+        updateCarousel();
+    });
+
+    updateCarousel();
 }
 
 function isValidLink(link){
@@ -95,7 +255,10 @@ async function scrape(link){
         elementsToRemove.forEach((element) => element.remove());
 
         // Retrieve the readable text content
-        const readableText = doc.body.textContent.trim();
+        let readableText = doc.body.textContent.trim();
+
+        // replace multiple tabs, spaces and newlines with single space
+        readableText = readableText.replace(/\s\s+/g, ' ').trim();
 
         return readableText;
     }
@@ -126,10 +289,10 @@ async function scrape(link){
     return { textContent, images }
 }
 
-async function summarize(text){
+async function summarize(selectedText, webText){
     const response = await new Promise((resolve) => {
         // Send a message to the background script to fetch the URL
-        chrome.runtime.sendMessage({ action: 'summarize', text: text }, (response) => {
+        chrome.runtime.sendMessage({ action: 'summarize', text: webText, selectedText: selectedText }, (response) => {
             resolve(response);
         });
     });
@@ -145,17 +308,21 @@ chrome.runtime.onMessage.addListener(async function(request, sender, sendRespons
 
         const selectedText = request.selectedText
 
-        links = request.metaphor.results
+        const links = request.metaphor.results
 
         const {tooltipContainer, linkList} = createLinkContainerEl(currEl)
 
         links.forEach((link) => {
-            if (!isValidLink(link)) return
-            linkItem =  createLinkEl(link)
+            if (!isValidLink(link)){
+                console.log('link too low a score')
+                console.log(link)
+                return
+            }
+
+            linkItem = createLinkEl(link)
             linkList.appendChild(linkItem);
         });
 
-        tooltipContainer.appendChild(linkList)
         currEl.appendChild(tooltipContainer)
 
         for (let i = 0; i < links.length; i++){
@@ -164,14 +331,23 @@ chrome.runtime.onMessage.addListener(async function(request, sender, sendRespons
             const { textContent, images } = await scrape(link)
 
             // split text into paragraphs, avoiding empty lines and lines with fewer than 10 words
-            const paragraphs = textContent.split('\n').filter(line => line.split(' ').length > 10).map((str) => str.trim())
-            const paragraphsReranked = await fetchRerank(selectedText, paragraphs)
+            // const paragraphs = textContent.split('\n').filter(line => line.split(' ').length > 10).map((str) => str.trim())
+            // const paragraphsReranked = await fetchRerank(selectedText, paragraphs)
+            // const ranks = JSON.parse(paragraphsReranked.ret).results
+            // const index = ranks[0].index
 
-            const ranks = JSON.parse(paragraphsReranked.ret).results
-
-            const index = ranks[0].index
-            let linkEl = tooltipContainer.querySelectorAll('.link-description')[i]
-            linkEl.textContent = paragraphs[index]
+            const response = await summarize(selectedText, textContent)
+            try {
+                const textContentSummary = JSON.parse(response)
+                let descriptionEl = tooltipContainer.querySelectorAll('.link-description')[i]
+                descriptionEl.textContent = textContentSummary.summary
+                let linkEl = tooltipContainer.querySelectorAll('.link-item')[i]
+                createTagEls(linkEl, textContentSummary.keywords)
+                createCarouselEl(linkEl, textContentSummary.passages)
+            }
+            catch (error) {
+                console.log(error)
+            }
         }
     }
 })
--- extension/eventPage.js
+++ extension/eventPage.js
@@ -10,38 +10,67 @@ chrome.contextMenus.create(contextMenuItem);
 
 const apikey_metaphor = '79c84cb7-8f8c-446e-a819-a9419ea29e46'
 const apikey_cohere = 'dXvUkT3DntsiIOhqRU8zDuoEPmE7irjk9z2seDZu'
+const apikey_openai = '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'
 
-function metaphor(query, numResults=10, callback){
+async function autoPrompt(query){
+    // https://production.metaphor.systems/api/autoprompt
+    const payload = {
+        query: query
+    }
+
     const options = {
         method: 'POST',
         headers: {
-          accept: 'text/plain',
+            'accept': 'text/plain',
+            'content-type': 'application/json',
+            'x-api-key': apikey_metaphor
+        },
+        body: JSON.stringify(payload)
+    };
+
+    const result = await fetch('https://api.metaphor.systems/autoprompt', options)
+    const json = await result.json()
+    return json
+}
+
+async function fetchMetaphor(query, numResults=10){
+    const prompt = "Excerpt: " + query + "\n\n Some great articles illustrating this point:"
+
+    const payload = {
+        query: prompt,
+        numResults: numResults
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+          'accept': 'text/plain',
           'content-type': 'application/json',
           'x-api-key': apikey_metaphor
         },
-        body: JSON.stringify({query: query, numResults: numResults})
+        body: JSON.stringify(payload)
       };
 
-      fetch('https://api.metaphor.systems/search', options)
-        .then(response => response.json())
-        .then(response => callback(response))
-        .catch(err => console.error(err));
-
+    const result = await fetch('https://api.metaphor.systems/search', options)
+    const json = await result.json()
+    return json
 }
 
-chrome.contextMenus.onClicked.addListener(function(clickData){
-    const selectedText = clickData.selectionText
-    if(clickData.menuItemId == "AIsearch" && selectedText){
-        // make a request to local search server
-        metaphor(selectedText, numMetaphorResults, function(response){
-            console.log(response)
-            // send the response back to the content script
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-                chrome.tabs.sendMessage(tabs[0].id, {metaphor: response, selectedText: selectedText})
-            })
-        })
+chrome.contextMenus.onClicked.addListener(async function (clickData) {
+    const selectedText = clickData.selectionText;
+    if (clickData.menuItemId == "AIsearch" && selectedText) {
+      try {
+        // Make a request to the local search server
+        const response = await fetchMetaphor(selectedText, numMetaphorResults);
+        // Send the response back to the content script
+        const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+        chrome.tabs.sendMessage(tabs[0].id, { metaphor: response, selectedText: selectedText });
+      } catch (error) {
+        console.error("Error fetching metaphor:", error);
+      }
     }
-});
+  });
+
 
 async function fetchURL(request, sendResponse){
     try {
@@ -83,12 +112,143 @@ async function fetchRerank(request, sendResponse){
     }
 }
 
+async function summarizeOpenAI(request, sendResponse){
+    const prompt = "Below is the text from a website. \
+    Respond with JSON containing a 2-sentence summary, 1-3 \
+    interesting short passages (verbatim) from the text, and 1-3 keywords describing the text. \
+    Use the following schema:\
+    {\"summary\": \"\", \"passages\": [\"\", \"\"], \"keywords\": [\"\", \"\"]}\
+    Website Text: "
+
+    const system_prompt = "You are an excellent summarizer, extracting interesting and relevant information. You only respond with valid JSON."
+
+    // truncate entire content if too long (max 4096 tokens). each word is about 2.5 tokens.
+    let content = prompt + request.text
+    if (content.split(' ').length > 4096/2.5) {
+        content = content.split(' ').slice(0, 4096/2.5).join(' ')
+    }
+
+    // Use openai gpt-3.5-turbo chat endpoint to summarize
+    try {
+        const payload = {
+            model: "gpt-3.5-turbo",
+            messages: [
+                {role: 'system', content: system_prompt},
+                {role: "user", content: content},
+            ],
+            temperature: 0
+        }
+
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apikey_openai}`
+            },
+            body: JSON.stringify(payload)
+        });
+        let ret = await response.text();
+        ret = JSON.parse(ret)['choices'][0].message.content
+        // Send the HTML content back to the content script
+        sendResponse(ret);
+    } catch (error) {
+        // Send an error message back to the content script
+        sendResponse({ error: error.message });
+    }
+}
+
+async function summarizeCohere(request, sendResponse){
+    // hit the summarize API
+    try {
+        const payload = {
+            text: request.text,
+            length: 'short',
+            format: 'paragraph',
+            model: 'summarize-xlarge',
+            additional_command: '',
+            temperature: 0,
+        }
+
+        const response_summary = await fetch('https://api.cohere.ai/v1/summarize', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apikey_cohere}`
+            },
+            body: JSON.stringify(payload)
+        });
+
+        const response_summary_text = await response_summary.text();
+        const summary = JSON.parse(response_summary_text).summary
+
+        // split text into sentences to use as the documents
+        const text = request.selectedText
+        const sentences = request.text.split('. ')
+
+        let documents = []
+        // combine pairs of sentences into documents
+        for (let i = 0; i < sentences.length; i += 2) {
+            if (i + 1 < sentences.length) {
+                documents.push(sentences[i] + '. ' + sentences[i + 1])
+            } else {
+                documents.push(sentences[i])
+            }
+        }
+
+        const payload_rerank = {
+            return_documents: false,
+            model: 'rerank-english-v2.0',
+            query: text,
+            documents: documents,
+            top_n: 3
+        }
+
+        const response_sentences = await fetch('https://api.cohere.ai/v1/rerank', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apikey_cohere}`
+            },
+            body: JSON.stringify(payload_rerank)
+        });
+
+        const ret_rerank = await response_sentences.text();
+        const results = JSON.parse(ret_rerank).results;
+
+        const passages = [
+            documents[results[0].index],
+            documents[results[1].index],
+            documents[results[2].index]
+        ]
+
+        let response = {
+            "summary": summary,
+            "passages": passages,
+            "keywords": []
+        }
+
+        let response_str = JSON.stringify(response)
+
+        // Send the HTML content back to the content script
+        sendResponse(response_str);
+    } catch (error) {
+        // Send an error message back to the content script
+        debugger
+        sendResponse({ error: error.message });
+    }
+}
+
 // background.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'fetchUrl')
         fetchURL(request, sendResponse)
-    else if (request.action === 'fetchRerank'){
+    else if (request.action === 'fetchRerank')
         fetchRerank(request, sendResponse)
-    }
+    else if (request.action === 'summarize')
+        // summarizeOpenAI(request, sendResponse)
+        summarizeCohere(request, sendResponse)
     return true
 });
